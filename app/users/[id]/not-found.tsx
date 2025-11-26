3306e9be0fade503936a1731ee68e1facd41d54b
Mohd_Shahwaz <mohdshahwaz9305@gmail.com>
2025-11-26 18:47:42 +0530

    Handling Not Found Errors

---
+++ app/users/[id]/not-found.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFoundUser = () => {
+  return (
+    <div>
+      <h1>This User doesn't exist</h1>
+    </div>
+  )
+}
+
+export default NotFoundUser
--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { notFound } from 'next/navigation';
 import React from 'react'
 interface Props{
   params:{
@@ -7,6 +8,7 @@ interface Props{
 const UserDetailsPage = async ( {params}:Props) => {
   
   const {id} = await params
+  if(id>10) notFound()
   return (
     
     <div>
