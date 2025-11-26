c6ca49bb9bcb1b8f569cc0df4e40289952123c33
Mohd_Shahwaz <mohdshahwaz9305@gmail.com>
2025-11-26 19:00:53 +0530

    Handling Unexpected Errors

---
+++ app/error.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react'
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <div>An unexpected error has occurred.</div>
+    </div>
+  )
+}
+
+export default ErrorPage
