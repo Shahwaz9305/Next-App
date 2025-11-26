9dd3a975e1565861fffde128548d0dbe7c73b436
Mohd_Shahwaz <mohdshahwaz9305@gmail.com>
2025-11-26 12:17:51 +0530

    Routing Overview

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,34 +1,13 @@
 import React from 'react'
-interface User{
-  id:number;
-  name:string;
-  email:string;
-}
+import UsersTable from './UsersTable'
+
 const User = async() => {
-const res=  await fetch("https://jsonplaceholder.typicode.com/users",{
-  cache:'no-cache'
-});
-const Users :User[] = await res.json();
+
   return (
     <div>
       <h1>Users</h1>
       <p>{new Date().toLocaleTimeString()}</p>
-      <table className='table table-bordered'>
-        <thead>
-            <tr>
-              <th>Name</th>
-              <th>Email</th>
-            </tr>
-        </thead>
-        <tbody>
-          {Users.map(user=> <tr key={user.id}> 
-            <td>{user.name}</td>
-            <td>{user.email}</td></tr> )}
-         
-        </tbody>
-        
-      </table>
-
+      <UsersTable/>
     </div>
   )
 }
