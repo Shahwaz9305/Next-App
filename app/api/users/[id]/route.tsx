533a96f06f31f29780e469484ed9618ae285a92f
Mohd_Shahwaz <mohdshahwaz9305@gmail.com>
2025-12-01 12:57:30 +0530

    Updating an Object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,3 +1,4 @@
+import { error } from "console";
 import { NextRequest, NextResponse } from "next/server";
 interface Props{
   params:{
@@ -8,4 +9,17 @@ export async function GET(request: NextRequest,{params}:Props){
 const {id}=await params;
 if(id>10)return NextResponse.json({error:'User not found'},{status:404});
 return NextResponse.json({id:1,name:'om'})
+}
+
+export async function PUT(request:NextRequest,{params}:Props) {
+  const {id} = await params;
+  const body = await request.json();
+  if(!body.name){
+    return NextResponse.json({error:'name is required'},{status:400})
+  }
+  if(id>10){
+    return NextResponse.json({error:'User not found'},{status:404})
+  }
+  return NextResponse.json({id:1,name:body.name});
+  
 }
